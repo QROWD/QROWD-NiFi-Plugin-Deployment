63f8da14d058ce0b983931fc29458ed29fc7603d
BonaBeavis <gordian.dziwis@gmail.com>
2018-08-22 15:45:08 +0200

    Put all vars in .env

--- setup-keystore.sh
+++ setup-keystore.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-initialAdmin="/CN=Claus Stadler/OU=People/DC=aksw/DC=org"
-nifiAdmin="CN=nifi"
+# Export the vars in .env into your shell:
+export $(egrep -v '^#' .env | xargs)
 
-keyPass="changeit"
-storePass="changeit"
+target="certs"
 
-#/CN=NiFi Admin/C=DE/L=Leipzig
-
-target="/certs"
 mkdir -p "$target"
 
-openssl req -x509 -newkey rsa:2048 -keyout "$target/admin-private-key.pem" -out "$target/admin-cert.pem" -days 365 -subj "$initialAdmin" -nodes
+openssl req -x509 -newkey rsa:2048 -keyout "$target/admin-private-key.pem" -out "$target/admin-cert.pem" -days 365 -subj "$CSR_SUBJ" -nodes
 
-openssl pkcs12 -inkey "$target/admin-private-key.pem" -in "$target/admin-cert.pem" -export -out "$target/admin.pfx" -passout pass:"$keyPass"
+openssl pkcs12 -inkey "$target/admin-private-key.pem" -in "$target/admin-cert.pem" -export -out "$target/admin.pfx" -passout pass:"$KEYSTORE_PASSWORD"
 
-keytool -genkeypair -alias nifiserver -keyalg RSA -keypass "$keyPass" -storepass "$storePass" -keystore "$target/keystore.jks" -dname "$nifiAdmin" -noprompt
+keytool -genkeypair -alias nifiserver -keyalg RSA -keypass "$KEYSTORE_PASSWORD" -storepass "$TRUSTSTORE_PASSWORD" -keystore "$target/keystore.jks" -dname "$INITIAL_ADMIN_IDENTITY" -noprompt
 
-keytool -importcert -v -trustcacerts -alias admin -file "$target/admin-cert.pem" -keystore "$target/truststore.jks" -storepass "$storePass" -noprompt
+keytool -importcert -v -trustcacerts -alias admin -file "$target/admin-cert.pem" -keystore "$target/truststore.jks" -storepass "$TRUSTSTORE_PASSWORD" -noprompt
 
