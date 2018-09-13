70abed0b0724a7899f27708be68aa3ed816ed924
BonaBeavis <gordian.dziwis@gmail.com>
2018-09-13 20:03:28 +0200

    Fix ssl??

--- setup/setup-keystore.sh
+++ setup/setup-keystore.sh
@@ -11,6 +11,6 @@ openssl req -x509 -newkey rsa:2048 -keyout "$target/admin-private-key.pem" -out 
 
 openssl pkcs12 -inkey "$target/admin-private-key.pem" -in "$target/admin-cert.pem" -export -out "$target/admin.pfx" -passout pass:"$KEYSTORE_PASSWORD"
 
-keytool -genkeypair -alias nifiserver -keyalg RSA -keypass "$KEYSTORE_PASSWORD" -storepass "$TRUSTSTORE_PASSWORD" -keystore "$target/keystore.jks" -dname "$INITIAL_ADMIN_IDENTITY" -noprompt
+keytool -importkeystore -srcstorepass "$KEYSTORE_PASSWORD" -deststorepass "$KEYSTORE_PASSWORD" -destkeypass "$KEYSTORE_PASSWORD" -srckeystore "$target/admin.pfx" -keystore "$target/truststore.jks"
 
 keytool -importcert -v -trustcacerts -alias admin -file "$target/admin-cert.pem" -keystore "$target/truststore.jks" -storepass "$TRUSTSTORE_PASSWORD" -noprompt
